0a08e83ffb3a47f0c4e9d95cd0f9f05ad4abec4c
zachary-foster <zacharyfoster1989@gmail.com>
2014-04-07 12:55:14 -0700

    minor

--- reformat_fasta.py
+++ reformat_fasta.py
@@ -16,11 +16,16 @@ import argparse
 
 #Functions
 
+class parse_phyto_id(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        
+        setattr(namespace, self.dest, values)
+
 #Command Line Parsing 
 command_line_parser = argparse.ArgumentParser(description=program_description, prefix_chars = "--")
-ommand_line_parser.add_argument('--phyto-id', metavar='STRING', default =  None, help='Default: none')
-ommand_line_parser.add_argument('--phyto-db', metavar='STRING', default =  None, help='Default: none')
-ommand_line_parser.add_argument('--rdp', metavar='STRING', default =  None, help='Default: none')
-ommand_line_parser.add_argument('--its1', metavar='STRING', default =  None, help='Default: none')
+ommand_line_parser.add_argument('--phyto-id', action=parse_phyto_id, help='Phytophthora ID format')
+ommand_line_parser.add_argument('--phyto-db', metavar='STRING', default =  None, help='Phytophthora DB format')
+ommand_line_parser.add_argument('--unite', metavar='STRING', default =  None, help='RDP/UNITE format')
+ommand_line_parser.add_argument('--its1', metavar='STRING', default =  None, help='ITS1 format')
 arguments = command_line_parser.parse_args()
 
