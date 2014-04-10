8d6af61760b282c1d152127c94002e3ea5cc1453
fosterz <zacharyfoster1989@gmail.com>
2014-04-10 16:01:33 -0700

    probably working

--- archive_website.py
+++ archive_website.py
@@ -2,17 +2,29 @@
 version = '0.0.1'
 change_log = [('0.0.1',  'First version of the script')]
 
-#Imports
+#Generic imports
 import os, sys, time
 import argparse
+
+#Specific imports
+from backup_website_constants import *
 
 #make time stamp
 date = time.strftime("%Y-%m-%d")
 epoch_time = str(time.time()).replace('.','-')
-time_stamp = '%s_%s' % (date, epoch_time)
+time_stamp = '%s_%s' % (epoch_time, date)
 
-#copy the local backup and add time stamp
-
-#compress local backup
+#tar the local backup and add time stamp to folder name
+archive_path = '%s_%s.tar.gz' % (target_path, time_stamp)
+tar_command = 'tar -zcf %s %s' % (archive_path, target_path)
+os.system(tar_command)
 
 #remove old backups if there are too many
+backups = [os.path.join(target_directory, item) for item in os.listdir(target_directory) if item.endswith('.tar.gz')]
+if len(backups) > number_of_backups_to_keep:
+	backups.sort(reverse=True)
+	number_to_delete = len(backups) - number_of_backups_to_keep
+	backups_to_delete = backups[-number_to_delete:]
+	for backup_path in backups_to_delete:
+		rm_command = 'rm -f %s' % (backup_path)
+		os.system(rm_command)
--- backup_website.py
+++ backup_website.py
@@ -4,25 +4,9 @@ change_log = [('0.0.1',  'First version of the script')]
 
 #Generic Imports
 import os, sys, time
-from subprocess import call
 
-#Constants
-port = 732
-user = 'fosterz'
-server = 'oomy.cgrb.oregonstate.edu'
-mysql_user = 'PhytoUser'
-mysql_password = '1nf3st4ns'
-target_directory = '/home/local/USDA-ARS/fosterz/website_backups/grunwald_lab'
-source_directory = '/data/www/grunwaldlab/drupal'
-mysql_name = 'grunwald_Drupal'
-remote_temporary_directory = '/tmp'
-
-#Parameters
-date = time.strftime("%Y-%m-%d")
-epoch_time = str(time.time()).replace('.','-')
-archive_name = 'grunwald_lab_website_backup'
-mysqldump_result_file = '%s_dump.sql' % (os.path.join(remote_temporary_directory, mysql_name) 
-target_path = os.path.join(target_directory, archive_name) + '/'
+#Specific imports
+from backup_website_constants import *
 
 #Dump MySQL database
 mysqldump_command = "ssh -p %d %s@%s 'mysqldump --user=%s --password=%s --result-file=%s %s'" %\
@@ -31,12 +15,12 @@ print(mysqldump_command)
 mysqldump_return_code = os.system(mysqldump_command)
 					 
 #Copy the dumped database to the local computer
-rsync_dump_command = "rsync -aczL --rsh='ssh -p %s' %s@%s:%s %s" % (port, user, server, mysqldump_result_file, target_path)
+rsync_dump_command = "rsync -aczL --rsh='ssh -p %s' %s@%s:%s %s" % (port, user, server, mysqldump_result_file, target_path + '/')
 print(rsync_dump_command)
 rsync_dump_return_code = os.system(rsync_dump_command)
 
 #Copy the drupal directory to the local computer
-rsync_drupal_command = "rsync -aczL --rsh='ssh -p %s' %s@%s:%s %s" % (port, user, server, source_directory, target_path)
+rsync_drupal_command = "rsync -aczL --rsh='ssh -p %s' %s@%s:%s %s" % (port, user, server, source_directory, target_path + '/')
 print(rsync_drupal_command)
 rsync_drupal_return_code = os.system(rsync_drupal_command)
 
