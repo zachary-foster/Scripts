09d3055a6f52eee04e65f9cfca418abbdaa5db48
fosterz <zacharyfoster1989@gmail.com>
2014-04-21 11:52:46 -0700

    added output folder overwrite ability

--- SGE_submission_scripts/sge_parallel.py
+++ SGE_submission_scripts/sge_parallel.py
@@ -17,26 +17,39 @@ default_free_memory = '10G'
 default_output_suffix = '_sge_parallel_output'
 default_error_suffix = '_sge_parallel_output'
 
-#Functions
-
 #Command Line Parsing 
 command_line_parser = argparse.ArgumentParser(description=program_description, prefix_chars = "--")
 command_line_parser.add_argument('input_file_path', metavar='INPUT_FILE_PATH', help='Path to a file containing commands to run.')
 command_line_parser.add_argument('--output', metavar='DIRECTORY_PATH',  default= None, help='A directory to store the standard output of the jobs. Default: Save in a new folder with a name derived from the input file plus "%s"' % default_output_suffix)
 command_line_parser.add_argument('--error', metavar='DIRECTORY_PATH', default = None, help='A directory to store the standard error of the jobs.Default: Save in a new folder with a name derived from the input file plus "%s"' % default_error_suffix)
 command_line_parser.add_argument('--mem_free', metavar='STRING', default =  default_free_memory, help='See qsub documentation ("man qsub"). Default: %s' % default_free_memory)
+command_line_parser.add_argument('--overwrite', metavar='BOOLEAN', action = 'store_true', default = False, help='Overwrite standard output and error files. Default: Add new files with old ones.')
 arguments = command_line_parser.parse_args()
-if arguments.error == None:
-	arguments.error = arguments.input_file_path + default_error_suffix
 if arguments.output == None:
 	arguments.output = arguments.input_file_path + default_output_suffix
+if arguments.error == None:
+	arguments.error = arguments.input_file_path + default_error_suffix
 
 #Count input lines 
 with open(arguments.input_file_path, 'r') as handle:
 	commands = handle.readlines()
 input_length = len(commands)
 	
+#Make output folders if they do not exist
+if not os.path.exists(arguments.output):
+	os.makedirs(arguments.output)
+elif not os.path.isdir(arguments.output):
+	print "WARNING: The path to the standard output ('%s') is a file, not a directory. This can result in garbeled output." % arguments.output
+elif arguments.overwrite:
+	shutil.rmtree(arguments.output + '/*')
 
+if not os.path.exists(arguments.error):
+	os.makedirs(arguments.error)
+elif not os.path.isdir(arguments.error):
+	print "WARNING: The path to the standard error ('%s') is a file, not a directory. This can result in garbeled output." % arguments.error
+elif arguments.overwrite:
+	shutil.rmtree(arguments.error + '/*')
+	
 #Make bash submission script
 qsub_input = r"""#!/bin/bash
 #$ -cwd
