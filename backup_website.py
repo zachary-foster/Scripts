02200a9338c574b274d7f585e0aff4bc96b096ea
fosterz <zacharyfoster1989@gmail.com>
2014-04-28 16:45:38 -0700

    intitial

--- backup_website.py
+++ backup_website.py
@@ -2,11 +2,25 @@
 version = '0.0.1'
 change_log = [('0.0.1',  'First version of the script')]
 
-#Generic Imports
+#Imports
 import os, sys, time
 
-#Specific imports
-from backup_website_constants import *
+#Constants
+port = 732
+user = 'fosterz'
+server = 'oomy.cgrb.oregonstate.edu'
+mysql_user = 'PhytoUser'
+mysql_password = '1nf3st4ns'
+target_directory = '/home/local/USDA-ARS/fosterz/website_backups/grunwald_lab'
+source_directory = '/data/www/grunwaldlab/drupal'
+mysql_name = 'grunwald_Drupal'
+remote_temporary_directory = '/export/mysql_dump'
+number_of_backups_to_keep = 10
+
+#Parameters
+archive_name = 'grunwald_lab_website_backup'
+mysqldump_result_file = '%s_dump.sql' % (os.path.join(remote_temporary_directory, mysql_name))
+target_path = os.path.join(target_directory, archive_name)
 
 #Dump MySQL database
 mysqldump_command = "ssh -p %d %s@%s 'mysqldump --user=%s --password=%s --result-file=%s %s'" %\
--- backup_website_constants.py
+++ backup_website_constants.py
@@ -10,7 +10,7 @@ mysql_password = '1nf3st4ns'
 target_directory = '/home/local/USDA-ARS/fosterz/website_backups/grunwald_lab'
 source_directory = '/data/www/grunwaldlab/drupal'
 mysql_name = 'grunwald_Drupal'
-remote_temporary_directory = '/tmp'
+remote_temporary_directory = '/export/mysql_dump'
 number_of_backups_to_keep = 10
 
 #Parameters
