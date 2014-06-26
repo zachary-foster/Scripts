d8f1040420a63ead5fa49aaa246092c9935a9a04
fosterz <zacharyfoster1989@gmail.com>
2014-06-25 18:11:27 -0700

    addded distance distribution

--- metacoder/calculate_threshold_statistics.R
+++ metacoder/calculate_threshold_statistics.R
@@ -32,8 +32,7 @@ if (distance_matrix[1,1] == 1) {
 taxonomy_data = read.csv(taxonomy_file, sep="\t", row.names=2, header=TRUE)
 
 #order taxonomic levels into an ordered factor based on order of occurance in stats file
-taxonomy_level_levels <- c(levels(taxonomy_data$level)[unique(taxonomy_data$level)])
-taxonomy_data$level <- ordered(taxonomy_data$level, taxonomy_level_levels)
+taxonomy_data$level <- ordered(taxonomy_data$level, levels=names(taxonomy_levels))
 
 #Prepare output directory
 if (!file.exists(output_directory)) {
@@ -71,96 +70,51 @@ intertaxon_statistics <- function(taxon, distance, identity, ...) {
               intertaxon_distance_count=sum(!is.na(inter_data))))
 }
 
-intrataxon_statistics <- function(taxon, distance, identity) {
-  if (all(is.na(unique(rownames(data))))) {
-    return(list(NA, NA, NA, NA))
+intrataxon_statistics <- function(taxon, distance, identity, ...) {
+  subtaxon_count <- length(unique(rownames(distance)))
+  if (subtaxon_count < 2) {
+    return(list(intrataxon_distance_mean=NA,
+                intrataxon_distance_sd=NA,
+                intrataxon_distance_count=NA,
+                subtaxon_count=NA))
   }
-  intra_data <- data[identity]
-  my_mean <- mean(intra_data, na.rm=TRUE)
-  my_sd <- sd(intra_data, na.rm=TRUE)
-  comparison_count <- sum(!is.na(intra_data))
-  taxon_count <- length(unique(rownames(data)))
-  return(list(my_mean, my_sd, comparison_count, taxon_count))
+  intra_data <- distance[identity]
+  return(list(intrataxon_distance_mean=mean(intra_data, na.rm=TRUE),
+              intrataxon_distance_sd=sd(intra_data, na.rm=TRUE),
+              intrataxon_distance_count=sum(!is.na(intra_data)),
+              subtaxon_count=subtaxon_count))
 }
 
-distance_graph_hist <- function(taxon, distance, identity) {
-  data$value <- remove_outliers(data$value)
-  taxon_name <- as.character(taxonomy_data[taxon, 'name'])
-  file_name <- paste(c(taxonomy_levels[as.character(taxonomy_data[taxon, 'level'])], '_', 
-                       taxon_name,
-                       '_', as.character(taxonomy_data[taxon, 'id']),
-                       '.png'), collapse="") 
-  sub_directory <- file.path(plot_directory, 'distance_distribution', fsep = .Platform$file.sep)
-  file_path <- file.path(sub_directory, file_name, fsep = .Platform$file.sep)
-  if (!file.exists(sub_directory)) {
-    dir.create(sub_directory, recursive=TRUE)
+distance_distribution <- function(taxon, distance, identity, distance_bin_width=0.001, ...) {
+  if (!is.matrix(distance) | sum(!is.na(distance)) == 0) {
+    return(list(distance_distribution=NA))
   }
-  png(file = file_path, bg = "transparent")
-  my_plot <-ggplot(data, aes(value)) + 
-    labs(title=taxon_name) +
-    theme(title=element_text(size=30),
-          axis.line=element_blank(),
-          axis.text.x=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks=element_blank(),
-          axis.title.x=element_blank(),
-          axis.title.y=element_blank(),
-          legend.position="none",
-          panel.background=element_blank(),
-          panel.border=element_blank(),
-          panel.grid.major=element_blank(),
-          panel.grid.minor=element_blank(),
-          plot.background=element_blank())
-  if (sum(data$relation == "Same") > 0) {
-    my_plot + geom_histogram(data=data[data$relation == "Same",], binwidth = .001, fill = 'magenta', alpha = .3, na.rm=TRUE)
-  }
-  if (sum(data$relation == "Different") > 0) {
-    my_plot + geom_histogram(data=data[data$relation == "Different",], binwidth = .001, fill = 'cyan', alpha = .3, na.rm=TRUE)
-  }
-  if (is.na(unique(data$relation))) {
-    my_plot + geom_histogram(data=data, binwidth = .001, fill = 'grey', alpha = .3, na.rm=TRUE)
+  #Calculate distance distribution
+  breaks <- seq(as.integer(min(distance, na.rm=TRUE) / distance_bin_width),
+                as.integer(max(distance, na.rm=TRUE) / distance_bin_width) + 1)
+  breaks <- breaks * distance_bin_width
+  total_hist <- hist(distance, plot=FALSE, breaks=breaks)
+  output <- data.frame(count_middle=total_hist$mids, total=total_hist$counts)
+  if (length(unique(rownames(distance))) >= 2) {
+    same_hist <- hist(distance[identity], plot=FALSE, breaks=breaks)
+    different_hist <- hist(distance[!identity], plot=FALSE, breaks=breaks)
+    output <- cbind(output, same=same_hist$counts, different=different_hist$counts)
   }
   
-  print(my_plot)
-  dev.off()
-  return(list(file_path))
-}
-
-distance_graph <- function(taxon, distance, identity) {
-  data <- melt(data, na.rm=TRUE)
-  data <- cbind(data, relation=ifelse(data$Var1 == data$Var2, "Same", "Different"))
-  data$value <- remove_outliers(data$value)
-  taxon_name <- as.character(taxonomy_data[taxon, 'name'])
-  file_name <- paste(c(taxonomy_levels[as.character(taxonomy_data[taxon, 'level'])], '_', 
-                       taxon_name,
-                       '_', as.character(taxonomy_data[taxon, 'id']),
-                       '.png'), collapse="") 
-  sub_directory <- file.path(plot_directory, 'distance_distribution', fsep = .Platform$file.sep)
+  #get output file path
+  file_name <- paste(c(taxonomy_levels[taxon$level], '_', 
+                       as.character(taxon$name),
+                       '_', as.character(taxon$id),
+                       '.txt'), collapse="") 
+  #prepare output directory
+  sub_directory <- file.path(output_directory, 'distance_distribution', fsep = .Platform$file.sep)
   file_path <- file.path(sub_directory, file_name, fsep = .Platform$file.sep)
   if (!file.exists(sub_directory)) {
     dir.create(sub_directory, recursive=TRUE)
   }
-  png(file = file_path, bg = "transparent")
-  print(ggplot(data, aes(value, fill=relation, y = ..scaled..)) + 
-          geom_density(alpha = .3, size=0, na.rm=TRUE, adjust = .3, position='identity') +
-          #          geom_density(aes(value, fill=NA), colour='lightgrey', na.rm=TRUE, adjust = .1, size=.8, position='identity') +
-          scale_fill_manual( values=c( "magenta","cyan" ) ) +
-          labs(title=taxon_name) +
-          theme(title=element_text(size=30),
-                axis.line=element_blank(),
-                #                axis.text.x=element_blank(),
-                axis.text.y=element_blank(),
-                axis.ticks.y=element_blank(),
-                axis.title.x=element_blank(),
-                axis.title.y=element_blank(),
-                legend.position="none",
-                panel.background=element_blank(),
-                panel.border=element_blank(),
-                panel.grid.major=element_blank(),
-                panel.grid.minor=element_blank(),
-                plot.background=element_blank()))
-  dev.off()
-  return(list(file_path))
+  #write output data
+  write.table(format(output, scientific = FALSE) , file=file_path, sep="\t", quote=FALSE, row.names=FALSE)
+  return(list(distance_distribution=file_path))
 }
 
 threshold_optimization_graph <- function(taxon, distance, identity) {
@@ -221,27 +175,64 @@ threshold_optimization_graph <- function(taxon, distance, identity) {
   return(list(file_path, optimal_threshold, optimal_false_negative, optimal_false_positive, optimal_error))
 }
 
+# functions_to_apply <- list(overall_statistics = c("distance_mean", "distance_sd", "distance_count", "subsampled_count"), 
+#                            intertaxon_statistics = c("intertaxon_distance_mean", "intertaxon_distance_sd", "intertaxon_distance_count"),
+#                            intrataxon_statistics = c("intrataxon_distance_mean", "intrataxon_distance_sd", "intrataxon_distance_count", "subtaxon_count"),
+#                            distance_graph = c("distance_graph"),
+#                            threshold_optimization_graph = c("threshold_graph", "optimal_threshold", "optimal_false_negative", "optimal_false_positive", "optimal_error"))
+
+#apply functions to subsets of distance matrix for each taxon (CAN TAKE LONG TIME)
+filter_taxonomy_string <- function(taxon, min_level, max_level) {
+  my_levels <- levels(taxonomy_data[taxon, 'level'])
+  parsed_taxonomy <- sapply(unlist(strsplit(taxon, split=';', fixed=T)),
+                            strsplit, split='__', fixed=T)
+  filter <- sapply(parsed_taxonomy, function(x) ordered(x[1], my_levels) >= min_level & ordered(x[1], my_levels) <= max_level)
+  parsed_taxonomy <- parsed_taxonomy[filter]
+  paste(sapply(parsed_taxonomy, paste, collapse='__'), collapse=';')
+}
+
+subsample_by_taxonomy <- function(taxon, triangular=TRUE, level = 'subtaxon', max_subset=NA) {
+  base_level <- offset_ordered_factor(taxonomy_data[taxon, 'level'], 1)
+  if (level == 'subtaxon') {
+    level <- base_level
+  }
+  if (is.na(level) | taxonomy_data[taxon, 'level'] >= level) {
+    return(NA)
+  }
+  indexes <- grep(taxon, distance_matrix_taxonomy, value = FALSE, fixed = TRUE)
+  if (!is.na(max_subset) && length(indexes) > max_subset) {
+    indexes = sample(indexes, max_subset)
+  }
+  submatrix <- distance_matrix[indexes, indexes, drop = FALSE]
+  names <- distance_matrix_taxonomy[indexes]
+  names <- mapply(FUN=filter_taxonomy_string, names, MoreArgs=list(base_level, level))
+  row.names(submatrix) <- names
+  colnames(submatrix) <- names
+  if (triangular) {
+    submatrix[upper.tri(submatrix, diag=TRUE)] <- NA
+  }
+  return(submatrix)
+}
 
 functions_to_apply <- list("overall_statistics",
-                           "intertaxon_statistics")
+                           "intertaxon_statistics",
+                           "intrataxon_statistics",
+                           "distance_distribution")
 
 get_stat_function_args <- function(data_frame_row, ...) {
   distance <- subsample_by_taxonomy(data_frame_row$.rownames, ...)
   identity <-  sapply(rownames(distance), function(x) colnames(distance) == x)
-  list(data_frame_row$.rownames, distance, identity)
+  list(data_frame_row, distance, identity)
 }
-
 taxon_statistics <- fapply(taxonomy_data, functions_to_apply,
                            preprocessor = get_stat_function_args,
                            preprocessor_args = list(level = level_to_analyze, 
                                                     max_subset = max_sequences_to_compare),
-                           append=T)
+                           append=T, 
+                           distance_bin_width=0.001)
 
 
-
-#apply functions to subsets of distance matrix for each taxon (CAN TAKE LONG TIME)
-taxon_statistics <- lapply(row.names(taxonomy_data), FUN=apply_functions, functions_to_apply, level=level_to_analyze, max_subset=1000)
-taxon_statistics <- ldply(taxon_statistics, data.frame)
+#Calculate statistics derived from other statistics
 taxon_statistics$inter_intra_differnece <- taxon_statistics$intertaxon_distance_mean - taxon_statistics$intrataxon_distance_mean
 taxon_statistics$optimal_error <- (taxon_statistics$optimal_false_negative + taxon_statistics$optimal_false_positive) / taxon_statistics$subsampled_count
 
--- metacoder/functions.R
+++ metacoder/functions.R
@@ -74,38 +74,6 @@ offset_ordered_factor <- function(ordered_factor, offset) {
   ordered(new_level, my_levels)
 }
 
-filter_taxonomy_string <- function(taxon, min_level, max_level) {
-  my_levels <- levels(taxonomy_data[taxon, 'level'])
-  parsed_taxonomy <- sapply(unlist(strsplit(taxon, split=';', fixed=T)),
-                            strsplit, split='__', fixed=T)
-  filter <- sapply(parsed_taxonomy, function(x) ordered(x[1], my_levels) >= min_level & ordered(x[1], my_levels) <= max_level)
-  parsed_taxonomy <- parsed_taxonomy[filter]
-  paste(sapply(parsed_taxonomy, paste, collapse='__'), collapse=';')
-}
-
-subsample_by_taxonomy <- function(taxon, triangular=TRUE, level = 'subtaxon', max_subset=NA) {
-  base_level <- offset_ordered_factor(taxonomy_data[taxon, 'level'], 1)
-  if (level == 'subtaxon') {
-    level <- base_level
-  }
-  if (is.na(level) | taxonomy_data[taxon, 'level'] >= level) {
-    return(NA)
-  }
-  indexes <- grep(taxon, distance_matrix_taxonomy, value = FALSE, fixed = TRUE)
-  if (!is.na(max_subset) && length(indexes) > max_subset) {
-    indexes = sample(indexes, max_subset)
-  }
-  submatrix <- distance_matrix[indexes, indexes, drop = FALSE]
-  names <- distance_matrix_taxonomy[indexes]
-  names <- mapply(FUN=filter_taxonomy_string, names, MoreArgs=list(base_level, level))
-  row.names(submatrix) <- names
-  colnames(submatrix) <- names
-  if (triangular) {
-    submatrix[upper.tri(submatrix, diag=TRUE)] <- NA
-  }
-  return(submatrix)
-}
-
 fapply <- function(data_frame, functions, preprocessor={function(x) x}, preprocessor_args=list(), append=FALSE, ...) {
   apply_functions <- function(input, functions, ...) {
     input <- append(input, list(...))
@@ -122,7 +90,6 @@ fapply <- function(data_frame, functions, preprocessor={function(x) x}, preproce
   }
   return(output)
 }
-
 
 remove_outliers <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.01, .99), na.rm = na.rm, ...)
